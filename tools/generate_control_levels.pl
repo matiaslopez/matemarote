fddd49dbe8236f84f6610977dc82c80694ed1975
meliascosta <meliascosta@2049f4cc-8395-11dd-90a9-6306d3ffb32f>
2009-04-23 23:58:15 +0000

    idem

--- tools/generate_control_levels.pl
+++ tools/generate_control_levels.pl
@@ -1,18 +1,32 @@
 #!/usr/bin/perl
 
-my $N = 150;
-my $archivo_string="gamefile ={\"max_incorrectas\":1, \"trials\":[";
+my $N = 8;
+my $archivo_string='gamefile =[';
 my @tipos = ('forward','inverse');
+my @orientaciones =('right','left');
+my $trial = '{"nro_flechas":15, "nro_tries":3,"tries":[';
 for (my $i=0;$i<=$N;$i++){
-    my $tiempo = int(2000 - $i/$N*1400);
-    my $kind = $tipos[int(rand()>(1-$i/$N*0.7))];
-    if ($i%30 == 0){
-        $archivo_string = $archivo_string."{\"kind\":\"$kind\",\"time\":$tiempo,\"checkpoint\":true},";
-    }else{
-        $archivo_string = $archivo_string."{\"kind\":\"$kind\",\"time\":$tiempo},";
-    }
+
+    $archivo_string = $archivo_string.$trial;
+    
+    for (my $j = 0; $j<=2;$j++){
+        $archivo_string = $archivo_string.'[';
+        for(my $k = 0; $k<=14;$k++){
+            my $tiempo = int(2000 - $i/$N*1400);
+            my $kind = $tipos[int(rand()>(1-$i/$N*0.7))];
+            my $posicion = int(rand()*90+1);
+            my $orientacion = $orientaciones[int(rand()>0.5)];
+            $archivo_string = $archivo_string."{\"kind\":\"$kind\",\"time\":$tiempo, \"posicion\": $posicion,\"orientacion\": \"$orientacion\"},";           
+        }
+        $archivo_string =~ s/,$//;
+        $archivo_string = $archivo_string.'],';
+    } 
+    $archivo_string =~ s/,$//;
+    $archivo_string = $archivo_string.']},';
+
 }
-$archivo_string = $archivo_string."]}";
+$archivo_string =~ s/,$//;
+$archivo_string = $archivo_string."]";
 
 open(OUT,">control_gamefile.txt") or die("nop pude");
 print OUT $archivo_string;
